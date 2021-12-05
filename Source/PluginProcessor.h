f83ee9b97ad38c159559395d9909f0d0c98277b5
Thomas Boggs <thomboggs@gmail.com>
2021-12-05 13:03:10 -0500

    Test For DSP module addition

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
 
 class AudioPluginAudioProcessor : public juce::AudioProcessor
 {
@@ -35,5 +36,7 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+    juce::dsp::Gain<float> gain;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
 };
